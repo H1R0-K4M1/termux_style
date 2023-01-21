d0ba9632ff054d2db6cfdf34916cf25543cd34ab
João Vitor Alves <101155407+H1R0Z1N@users.noreply.github.com>
2023-01-21 14:52:29 -0300

    Add files via upload

---
+++ instalar.sh
@@ -0,0 +1,34 @@
+b="\033[0m"
+B="${b}\033[34;1m"
+G="${b}\033[32;1m"
+x="${b}\033[30;1m"
+clear
+echo -e "
+   $x▒${B}█${x}░▒${B}█ ▀█▀ $x▒${B}█▀▀█ $x▒${B}█▀▀▀█ 　 $x▒${B}█${x}░${B}▄▀ $x░${B}█▀▀█ $x▒${B}█▀▄▀█ ▀█▀
+   $x▒${B}█▀▀█ $x▒${B}█${x}░ $x▒${B}█▄▄▀ $x▒${B}█${x}░░▒${B}█ 　 $x▒${B}█▀▄${x}░ $x▒${B}█▄▄█ $x▒${B}█${x}▒${B}█${x}▒${B}█ $x▒${B}█${x}░
+   $x▒${B}█${x}░▒${B}█ ▄█▄ $x▒${B}█${x}░▒${B}█ $x▒${B}█▄▄▄█ 　 $x▒${B}█${x}░▒${B}█ $x▒${B}█${x}░▒${B}█ $x▒${B}█${x}░░▒${B}█ ▄█▄
+   instagram:${G} the_hiro_kami
+   "
+
+read -p "Deseja instalar o Parrot Shell no Termux? (Yes /No): " input
+if [[ $input == Yes || $input == yes || $input == y || $input == Y ]]; then
+	sleep 3
+	cp bash.bashrc $HOME
+	rm -rf bash.bashrc
+	cd /data/data/com.termux/files/usr/etc
+	rm -rf motd
+	cd $HOME
+	mv bash.bashrc /data/data/com.termux/files/usr/etc
+	echo -e "\e[1;91mAbrindo instagram...\n"
+	sleep 3
+	xdg-open https://www.instagram.com/the_hiro_kami/ && sleep 3
+	echo -e "\e[1;91mInstalado com sucesso"
+	echo -e "Reinicie seu Termux"
+	exit 3
+
+elif [[ $input == No || $input == no || $input == n || $input == N ]]; then
+	exit 2
+else
+	echo -e "\e[1;91mOpção Invalida!"
+	exit
+fi
